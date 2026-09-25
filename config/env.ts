4237467ea8bdd6ef388f35bd97757e563b1ea96a
mobinaj94 <mobinajafari94@yahoo.com>
2026-09-26 00:08:52 +0200

    connect to supabase

--- config/env.ts
+++ config/env.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string().min(1),
+  DIRECT_URL: z.string().min(1),
   PORT: z.coerce.number().default(8008), //what we get from .env is string => we change it to number
   NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   ORIGIN:z.string()
